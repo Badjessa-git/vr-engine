5b139926c37089393ca9c71ccddc2e3c4061a20e
daninewbs <51922543+daninewbs@users.noreply.github.com>
2019-06-18 16:41:36 -0400

    Add files via upload
    comment out top part to get functionality of old recieveMessage script

--- recieveMessage.js
+++ recieveMessage.js
@@ -1,58 +1,63 @@
-// Receiving script.
-Messages.subscribe("101");
-Messages.messageReceived.connect(play);
 
-function play (channel, data, sender, localOnly){
-	if(data === "hi")
+var Mess = false;
+// default animation
+//change to FSM using distance from avatar as switch?
+function startAn(){
+	
+	var PLAYBACK_CHANNEL = "playbackChannel";
+	Recording.loadRecording("atp:/confusion_Record.hfr");
+
+	Recording.setPlayFromCurrentLocation(false);
+	Recording.setPlayerUseDisplayName(true);
+	Recording.setPlayerUseAttachments(true);
+	Recording.setPlayerUseHeadModel(false);
+	Recording.setPlayerLoop(false);
+	Recording.setPlayerUseSkeletonModel(true);
+
+	Agent.isAvatar = true;
+	if (!Recording.isPlaying()) 
 	{
-		var PLAYBACK_CHANNEL = "playbackChannel";
-
-		Recording.loadRecording("atp:/fall.hfr");
-
-		Recording.setPlayFromCurrentLocation(false);
-		Recording.setPlayerUseDisplayName(true);
-		Recording.setPlayerUseAttachments(true);
-		Recording.setPlayerUseHeadModel(false);
-		Recording.setPlayerLoop(false);
-		Recording.setPlayerUseSkeletonModel(true);
-
-		Agent.isAvatar = true;
-		if (!Recording.isPlaying()) 
-		{
-			//robo
-			Recording.setPlayerTime(0.0);
-			Recording.startPlaying();
-			
-		}
-		Script.update.connect(update);
+		//woody
+		Recording.setPlayerTime(0.0);
+		Recording.startPlaying();
+		
 	}
-	if(data === "fi")
-	{
-		var PLAYBACK_CHANNEL = "playbackChannel";
-
-		Recording.loadRecording("atp:/meh.hfr");
-
-		Recording.setPlayFromCurrentLocation(false);
-		Recording.setPlayerUseDisplayName(true);
-		Recording.setPlayerUseAttachments(true);
-		Recording.setPlayerUseHeadModel(false);
-		Recording.setPlayerLoop(false);
-		Recording.setPlayerUseSkeletonModel(true);
-
-		Agent.isAvatar = true;
-		if (!Recording.isPlaying()) 
-		{
-			//woody
-			Recording.setPlayerTime(0.0);
-			Recording.startPlaying();
-			
-		}
-		Script.update.connect(update);
-	}
+	
+}
+if (!Mess){
+	Script.update.connect(startAn); // believe the issue must lie in here 
 }
 
 
 
 
+// Receiving script.
+Messages.subscribe("101");
+Messages.messageReceived.connect(play);
 
 
+function play (channel, data, sender, localOnly){
+	var url;
+	Mess = true;
+	
+	if(data === "hi"){
+		url = "atp:/fall.hfr";
+	}	
+
+	if(data === "fi"){
+		url = "atp:/meh.hfr";
+	}
+	var PLAYBACK_CHANNEL = "playbackChannel";
+
+	Recording.loadRecording(url);
+	Recording.setPlayFromCurrentLocation(true);
+	Recording.setPlayerUseDisplayName(true);
+	Recording.setPlayerUseAttachments(true);
+	Recording.setPlayerLoop(false);
+	Agent.isAvatar = true;
+
+	Recording.setPlayerTime(0.0);
+	Recording.startPlaying();		
+
+	Script.update.connect(update);
+}
