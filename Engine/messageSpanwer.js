70b4630338bcc95cf138a7fd353bc8903d02ffdb
Alan Bettis <40707617+TheLostPxl56@users.noreply.github.com>
2019-06-21 11:56:41 -0400

    Can now delete message boxes on click

---
+++ Engine/messageSpanwer.js
@@ -0,0 +1,23 @@
+//Interface script to spawn interactable menus
+
+Messages.subscribe("entitySpawner");
+//Messages.messageReceived.connect(function(channel, data, sender, localOnly)
+//{
+	var position = Vec3.sum(MyAvatar.position, Quat.getFront(MyAvatar.orientation));
+	
+	var properties = {
+		type: "Text",
+		text: "Hello, world!",
+		position: position,
+		name: "testText",
+		dimensions: {x:1,y:1,z:0.1},
+		script: "atp:/textBoxScript.js"
+	};
+	
+	var entityID = Entities.addEntity(properties);
+	print("text spawn attempted");
+	
+Messages.messageReceived.connect(function (channel, message, senderID, localOnly) {
+    Entities.deleteEntity(message);
+});
+//});
