c5507199c9b363c212ab96490e451786e80220a3
Alan Bettis <40707617+TheLostPxl56@users.noreply.github.com>
2019-06-24 16:03:32 -0400

    Now allows text input from engine

--- Engine/messageSpawner.js
+++ Engine/messageSpawner.js
@@ -10,23 +10,45 @@ var text3 = "Goodbye, world!";
 	
 Messages.dataReceived.connect(function (channel, data, senderID, localOnly) {
 	var rotation = MyAvatar.orientation;
-	var position = Vec3.sum(MyAvatar.position, Quat.getFront(rotation));
-	//var text = data;
+	var left = Vec3.sum(MyAvatar.position, Quat.getLeft(rotation));
+	var front = Vec3.sum(MyAvatar.position, Quat.getFront(rotation));
+	var right = Vec3.sum(MyAvatar.position, Quat.getRight(rotation));
+	text1 = data[0];
+	text2 = data[1];
+	text3 = data[2];
 	
 	var properties1 = {
 		type: "Text",
 		text: text1,
-		position: position,
+		position: left,
 		rotation: rotation,
 		name: "testText",
 		dimensions: {x:1,y:0.1,z:0.1},
 		script: "atp:/textBoxScript.js"
 	};
 	
-	var properties2 = // adjust positioning and ref text2
+	var properties2 = {
+		type: "Text",
+		text: text2,
+		position: front,
+		rotation: rotation,
+		name: "testText",
+		dimensions: {x:1,y:0.1,z:0.1},
+		script: "atp:/textBoxScript.js"
+	};
 	    
-	var properties3 = // adjust positioning and ref text3
+	var properties3 = {
+		type: "Text",
+		text: text3,
+		position: right,
+		rotation: rotation,
+		name: "testText",
+		dimensions: {x:1,y:0.1,z:0.1},
+		script: "atp:/textBoxScript.js"
+	};
 	
-	var entityID = Entities.addEntity(properties);
+	var leftEntityID = Entities.addEntity(properties1);
+	var frontEntityID = Entities.addEntity(properties2);
+	var rightEntityID = Entities.addEntity(properties3);
 	print("text spawn attempted");
 });
