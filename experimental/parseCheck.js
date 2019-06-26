2e5a6a422d17e1dc70a5beba337d37371de860b5
Alan Bettis <40707617+TheLostPxl56@users.noreply.github.com>
2019-06-26 16:46:59 -0400

    Create parseCheck.js

---
+++ experimental/parseCheck.js
@@ -0,0 +1,62 @@
+//Interface script to spawn interactable menus
+
+//This channel will receive text values to display in text boxes
+Messages.subscribe("menuSystem");
+
+//Set default text
+var text1 = "Hello, world!";
+var text2 = "Evening, world!";
+var text3 = "Goodbye, world!";
+	
+Messages.messageReceived.connect(function (channel, unprocessedData, senderID, localOnly) {
+	//Check to make sure the message is from the engine
+	//if(unprocessedData === "check")
+	//{
+		var rotation = MyAvatar.orientation;
+		var left = Vec3.sum(MyAvatar.position, Quat.getUp(rotation));
+		var front = Vec3.sum(MyAvatar.position, Quat.getFront(rotation));
+		var right = Vec3.sum(MyAvatar.position, Quat.getRight(rotation));
+		var data = unprocessedData.split("|");
+		text1 = data[0];
+		text2 = data[1];
+		text3 = data[2];
+		
+		//Properties of message1
+		var properties1 = {
+			type: "Text",
+			text: text1,
+			position: left,
+			rotation: rotation,
+			name: "testText",
+			dimensions: {x:1,y:0.1,z:0.1},
+			script: "atp:/textBoxScript.js"
+		};
+		
+		//Properties of message2
+		var properties2 = {
+			type: "Text",
+			text: text2,
+			position: front,
+			rotation: rotation,
+			name: "testText",
+			dimensions: {x:1,y:0.1,z:0.1},
+			script: "atp:/textBoxScript.js"
+		};
+		
+		//Properties of message3
+		var properties3 = {
+			type: "Text",
+			text: text3,
+			position: right,
+			rotation: rotation,
+			name: "testText",
+			dimensions: {x:1,y:0.1,z:0.1},
+			script: "atp:/textBoxScript.js"
+		};
+		
+		var leftEntityID = Entities.addEntity(properties1);
+		var frontEntityID = Entities.addEntity(properties2);
+		var rightEntityID = Entities.addEntity(properties3);
+		print("text spawn attempted");
+	//}
+});
