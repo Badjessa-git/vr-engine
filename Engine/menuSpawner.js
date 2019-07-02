170bf5e3cae79e68c52828cdfc744e6cce37fd92
Alan Bettis <40707617+TheLostPxl56@users.noreply.github.com>
2019-07-02 16:29:54 -0400

    Add files via upload

--- Engine/menuSpawner.js
+++ Engine/menuSpawner.js
@@ -4,27 +4,38 @@
 Messages.subscribe("menuSystem");
 	
 Messages.messageReceived.connect(function (channel, unprocessedData, senderID, localOnly) {
-	//Check to make sure the message is from the engine
+	//Parse message from engine into 4 components
 	var data = unprocessedData.split("|");
-	var flag = data[0];
-	var text1 = data[1];
-	var text2 = data[2];
-	var text3 = data[3];
 
+	//flag to ensure that message is from the engine
+	var flag = data[0];
+	//text for first option
+	var text1 = data[1];
+	//text for second option
+	var text2 = data[2];
+	//text for third option
+	var text3 = data[3];
+	//text for fourth option
+	var text4 = data[4];
+
+	//Check if message is from engine
 	if(flag === "check")
 	{
+		//set up orientation and positions for the text boxes
 		var rotation = MyAvatar.orientation;
-		var left = Vec3.sum(MyAvatar.position, Quat.getUp(rotation));
-		var front = Vec3.sum(MyAvatar.position, Quat.getFront(rotation));
-		var right = Vec3.sum(MyAvatar.position, Quat.getRight(rotation));
+		var basePosition = Vec3.sum(MyAvatar.position, Quat.getFront(rotation));
+		var position1 = Vec3.sum(basePosition, {x:0, y:0.75, z:0});
+		var position2 = Vec3.sum(basePosition, {x:0, y:0.5, z:0});
+		var position3 = Vec3.sum(basePosition, {x:0, y:0.25, z:0});
+		var position4 = basePosition;
 		
 		//Properties of message1
 		var properties1 = {
 			type: "Text",
 			text: text1,
-			position: left,
+			position: position1,
 			rotation: rotation,
-			name: "testText",
+			name: "option1",
 			dimensions: {x:1,y:0.1,z:0.1},
 			script: "atp:/textBoxScript.js"
 		};
@@ -33,9 +44,9 @@ Messages.messageReceived.connect(function (channel, unprocessedData, senderID, l
 		var properties2 = {
 			type: "Text",
 			text: text2,
-			position: front,
+			position: position2,
 			rotation: rotation,
-			name: "testText",
+			name: "option2",
 			dimensions: {x:1,y:0.1,z:0.1},
 			script: "atp:/textBoxScript.js"
 		};
@@ -44,16 +55,28 @@ Messages.messageReceived.connect(function (channel, unprocessedData, senderID, l
 		var properties3 = {
 			type: "Text",
 			text: text3,
-			position: right,
+			position: position3,
 			rotation: rotation,
-			name: "testText",
+			name: "option3",
 			dimensions: {x:1,y:0.1,z:0.1},
 			script: "atp:/textBoxScript.js"
 		};
 		
-		var leftEntityID = Entities.addEntity(properties1);
-		var frontEntityID = Entities.addEntity(properties2);
-		var rightEntityID = Entities.addEntity(properties3);
-		print("text spawn attempted");
+		//Properties of message4
+		var properties4 = {
+			type: "Text",
+			text: text4,
+			position: position3,
+			rotation: rotation,
+			name: "option4",
+			dimensions: {x:1,y:0.1,z:0.1},
+			script: "atp:/textBoxScript.js"
+		};
+		
+		//spawn textboxes
+		var textBoxID1 = Entities.addEntity(properties1);
+		var textBoxID2 = Entities.addEntity(properties2);
+		var textBoxID3 = Entities.addEntity(properties3);
+		var textBoxID4 = Entities.addEntity(properties4);
 	}
 });
