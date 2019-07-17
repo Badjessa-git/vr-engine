fe2021f4edbdf058c6efd0df5eaa199df7f06530
Alan Bettis <40707617+TheLostPxl56@users.noreply.github.com>
2019-07-17 17:23:09 -0400

    Testing js objects instead of functions for engine

--- Engine/engineCore.js
+++ Engine/engineCore.js
@@ -1,161 +1,29 @@
-//Finite State Machine
+/*
+CONTENTS
 
-//Urls for animations
-var firstAnimationURL = "atp:/20190702-203823.hfr";
-var secondAnimationURL = "atp:/20190708-201920.hfr";
+Basic setup
+Execute State
+Animation functions
+Audio functions
+*/
 
-var activeAnimationURL;
-var idleAnimationURL;
-
-//Text for menu options
-var activeMenuOptions;
-
-//Audio for menu options
-var dialogueAudio = ["atp:/Audio/Option1.mp3", 
-		     "atp:/Audio/Option2.mp3", 
-		     "atp:/Audio/Option3.mp3", 
-		     "atp:/Audio/Option4.mp3"];
+/*************************BASIC SETUP*************************/
+//activeState will be referenced by the main controller function
+var activeState = state0;
 
 //specify counter to keep track of animation cycles
 var count = 0;
 
-//Subscribe to message channel
+//Subscribe to message channel to receive notices from other scripts
 Messages.subscribe("engine");
 
-//Initial state requires that there be a higher count limit. 
-//Reasons unknown. Possibly due to running during load?
-//This funciton is in place to circumvent this issue.
-function initialAnimationHelper()
-{
-	Script.update.connect(play);
-}
-
-//Helper function for animation player
-function animationHelper()
-{
-	count = 1;
-	Script.update.connect(play);
-}
-
-//play animation
-function play()
+/*************************EXECUTE STATE*************************/
+function executeState()
 {
 	//log
-	print("animation controller called");
-	
-	var PLAYBACK_CHANNEL = "playbackChannel";
-	Recording.loadRecording(activeAnimationURL);
-
-	Recording.setPlayFromCurrentLocation(false);
-	Recording.setPlayerUseDisplayName(true);
-	Recording.setPlayerUseAttachments(true);
-	Recording.setPlayerUseSkeletonModel(true);
-
-	Agent.isAvatar = true;
-	if (!Recording.isPlaying())
-	{
-		Recording.setPlayerTime(0.0);
-		Recording.startPlaying();
-		count++;
-		
-		if(count > 2)
-		{ 
-			menuSpawner(activeMenuOptions);
-			Recording.stopPlaying();
-			count = 0;
-			Script.update.disconnect(play);
-		}
-	}
-}
-
-//Read all options in sequence recursively
-function readOptions(index)
-{
-	//Base case, array of urls has been iterated through
-	if(index === dialogueAudio.length)
-	{
-		//send message to unlock text boxes
-		Messages.sendMessage("readingNotice", "toggle lock");
-		return;
-	}
-	
-	sound = SoundCache.getSound(dialogueAudio[index]);
-	function playSound() 
-	{
-	    var injector = Audio.playSound(sound);
-		
-	    //connect signal to trigger next audio
-	    injector.finished.connect(function recurse()
-	    {
-	    	injector.finished.disconnect(recurse);
-			readOptions(++index);
-	    });
-	}
-
-	function onSoundReady() 
-	{
-	    sound.ready.disconnect(onSoundReady);
-	    playSound();
-	}
-
-	if (sound.downloaded) 
-	{
-	    playSound();
-	} 
-	else 
-	{
-	    sound.ready.connect(onSoundReady);
-	}
-}
-
-//Read only one dialogue option
-	
-	sound = SoundCache.getSound(dialogueAudio[index]);
-	function playSound() 
-	{
-	    var injector = Audio.playSound(sound);
-		
-	    //connect signal to trigger next audio
-	    injector.finished.connect(function recurse()
-	    {
-	    	injector.finished.disconnect(recurse);
-	    });
-	}
-
-	function onSoundReady() 
-	{
-	    sound.ready.disconnect(onSoundReady);
-	    playSound();
-	}
-
-	if (sound.downloaded) 
-	{
-	    playSound();
-	} 
-	else 
-	{
-	    sound.ready.connect(onSoundReady);
-	}
-
-//spawn menu
-function menuSpawner(unprocessedData)
-{
-	//Send message to entity creator script
-	Messages.sendMessage("menuSystem", unprocessedData);
-	readOptions(0);
-}
-
-//----------------------------------------------------------------------------------------------------------------
-//state function declarations
-
-function initialState()
-{
-	//log
-	print("initialState entered");
+	print("State transition occured");
 	
 	//set animation parameters and play animation
-	activeAnimationURL = firstAnimationURL;
-	activeMenuOptions = "check|Go to state1|Go to state1|Go to state1|Go to state1";
 	initialAnimationHelper();
 	
 	//Test print
@@ -181,65 +49,161 @@ function initialState()
 	});
 }
 
-function state1()
+/*************************ANIMATION*************************/
+//Initial state requires that there be a higher count limit. 
+//Reasons unknown. Possibly due to running during load?
+//This funciton is in place to circumvent this issue.
+function initialAnimationHelper()
 {
-	//test log
-	print("Now entering state1");
+	Script.update.connect(play);
+}
 
-	//set animation parameters and play animation
-	activeAnimationURL = secondAnimationURL;
-	activeMenuOptions = "check|Go to state2|Go to state2|Go to state2|Go to state2";
-	animationHelper();
+//Helper function for animation player
+function animationHelper()
+{
+	count = 1;
+	Script.update.connect(play);
+}
+
+//play animation
+function play()
+{
+	//log
+	print("animation controller called");
 	
-	//listen for message to trigger transition
-	Messages.messageReceived.connect(function state1Listener(channel, message, senderID, localOnly)
+	var PLAYBACK_CHANNEL = "playbackChannel";
+	Recording.loadRecording(activeState.animationURL);
+
+	Recording.setPlayFromCurrentLocation(false);
+	Recording.setPlayerUseDisplayName(true);
+	Recording.setPlayerUseAttachments(true);
+	Recording.setPlayerUseSkeletonModel(true);
+
+	Agent.isAvatar = true;
+	if (!Recording.isPlaying())
 	{
-		//test print
-		print(message);
-		if(message == "\"0\"" || message == "\"1\"" || message == "\"2\"" || message == "\"3\"")
-		{
-			Messages.messageReceived.disconnect(state1Listener);
-			state2();
+		Recording.setPlayerTime(0.0);
+		Recording.startPlaying();
+		count++;
+		
+		if(count > 2)
+		{ 
+			menuSpawner(activeState.dialogue);
+			Recording.stopPlaying();
+			count = 0;
+			Script.update.disconnect(play);
 		}
-		//respond to repeat button
-		if(message == "repeatAudio")
+	}
+}
+/*************************AUDIO*************************/
+//Read all options in sequence
+function readOptions(audioURL1, audioURL2, audioURL3, audioURL4)
+{
+	//call playAudio to play the first dialogue audio 
+	var injector = playAudio(audioURL1);
+	//Connect to signal to play next audio after first is done
+	injector.finished.connect(function recurse()
+	{
+	   	injector.finished.disconnect(recurse);
+		//call playAudio to play the second dialogue audio 
+		injector = playAudio(audioURL2);
+		injector.finished.connect(function recurse()
 		{
-			//Send message to lock text boxes
-			Messages.sendMessage("readingNotice", "toggle lock");
-			readOptions(0);
-		}
+			injector.finished.disconnect(recurse);
+			//call playAudio to play the second dialogue audio 
+			injector = playAudio(audioURL3);
+			injector.finished.connect(function recurse()
+			{
+				injector.finished.disconnect(recurse);
+				//call playAudio to play the second dialogue audio 
+				injector = playAudio(audioURL4);
+				injector.finished.connect(function recurse()
+				{
+					injector.finished.disconnect(recurse);
+				});
+			});
+		});
 	});
 }
 
-function state2()
+//Play an MP3 file from a url in the current state's audio array
+function playAudio(audioURL)
 {
-	//test log
-	print("Now entering state2");
-
-	//set animation parameters and play animation
-	activeAnimationURL = firstAnimationURL;
-	activeMenuOptions = "check|Go to state1|Go to state1|Go to state1|Go to state1";
-	animationHelper();
-	
-	//listen for message to trigger transition
-	Messages.messageReceived.connect(function state2Listener(channel, message, senderID, localOnly)
+	sound = SoundCache.getSound(audioURL);
+	function playSound() 
 	{
-		//test print
-		print(message);
-		if(message == "\"0\"" || message == "\"1\"" || message == "\"2\"" || message == "\"3\"")
-		{
-			Messages.messageReceived.disconnect(state2Listener);
-			state1();
-		}
-		//respond to repeat button
-		if(message == "repeatAudio")
-		{
-			//Send message to lock text boxes
-			Messages.sendMessage("readingNotice", "toggle lock");
-			readOptions(0);
-		}
-	});
-}
+	    var injector = Audio.playSound(sound);
+		return injector;
+	}
 
-//Call initialState
-initialState();
+	function onSoundReady() 
+	{
+	    sound.ready.disconnect(onSoundReady);
+	    playSound();
+	}
+
+	if (sound.downloaded) 
+	{
+	    playSound();
+	} 
+	else 
+	{
+	    sound.ready.connect(onSoundReady);
+	}
+}
+/*************************STATE DEFINITIONS*************************/
+var state0
+{
+	//HF asset browser URL of animation to be played upon state entering
+	animationURL: "atp:/Animations/WalkTest1.hfr",
+	
+	//Response options to be presented to player
+	option1: {
+		//Text to be contained within option
+		dialogue: "option1",
+		//HF asset browser URL of voiceover for dialogue
+		audioURL: "atp:/Audio/Option1.mp3",
+		//Index of state to transition to when option is selected
+		transition: state1
+	},
+	option2: {
+		dialogue: "option2",
+		audioURL: "atp:/Audio/Option2.mp3",
+		transition: state1
+	},
+	option3: {
+		dialogue: "option3",
+		audioURL: "atp:/Audio/Option3.mp3",
+		transition: state1
+	},
+	option4: {
+		dialogue: "option4",
+		audioURL: "atp:/Audio/Option4.mp3",
+		transition: state1
+	}
+},
+var state1
+{
+	animationURL: "atp:/Animations/WalkTest2.hfr",
+	
+	option1: {
+		dialogue: "option1",
+		audioURL: "atp:/Audio/Option1.mp3",
+		transition: state0
+	},
+	option2: {
+		dialogue: "option2",
+		audioURL: "atp:/Audio/Option2.mp3",
+		transition: state0
+	},
+	option3: {
+		dialogue: "option3",
+		audioURL: "atp:/Audio/Option3.mp3",
+		transition: state0
+	},
+	option4: {
+		dialogue: "option4",
+		audioURL: "atp:/Audio/Option4.mp3",
+		transition: state0
+	}
+}
